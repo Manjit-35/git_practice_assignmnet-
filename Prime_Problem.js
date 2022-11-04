4a58871d3a94e5b86430ae3a986ad4558e1904c6
Manjit Singh <darkhack775@gmail.com>
2022-11-04 19:47:44 +0530

    code fix

--- Prime_Problem.js
+++ Prime_Problem.js
@@ -1,11 +1,11 @@
 function check_prime(N){
-    let fa="";
-    for(i=0;i<=N+1;i--){
-        if(N%i!==0){
+    let factor=0;
+    for(i=0;i<N+1;i++){
+        if(N%i===0){
             count++;
         }
     }
-    if(fa===2){
+    if(factor===2){
         conmsole.lag("Prime");
     }
     else{
